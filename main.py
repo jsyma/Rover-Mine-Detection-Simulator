99967ab270d31716900a79219f4def6ba87a5b8b
jsyma <johnnyma99@gmail.com>
2025-01-16 01:30:56 -0500

    Added write to path_rover_id.txt function and updated varaible naming

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import json
 
 
 def get_rover_commands(rover_count):
-    """
+    '''
     Gets the rover commands from the provided api
-    @param rover_count  The number of Rovers to get commands from
-    @return The Rover commands in JSON format
-    """
+
+    Args: 
+        rover_count (int): The number of rovers to get commands from.
+    
+    Returns: 
+        list: A list of Rover commands, each element in the list is a list of commands
+              for a specific rover.
+    '''
     commands = []
     api = 'https://coe892.reev.dev/lab1/rover'
     for rover_id in range(1, rover_count + 1):
@@ -21,67 +26,65 @@ def get_rover_commands(rover_count):
 
 def rover_movement(rover_id, commands, info, starter_map):
     map = starter_map
-    maxR = (int)(info[0])
-    maxC = (int)(info[1])-1
-    r = 0
-    c = 0
+    number_of_rows = int(info[0])
+    number_of_cols = int(info[1]) - 1
+    rover_row_pos = 0
+    rover_col_pos = 0
     current_direction = "SOUTH"
+
     for move in commands[rover_id]:
         if move == 'L' or move == 'R':
             current_direction = rotate_rover(current_direction, move)
         elif move == 'M':
-            if current_direction == "SOUTH":
-                if r+1 < maxR :
-                    r+=1
-            if current_direction == "NORTH":
-                if  r - 1 >= 0:
-                    r -=1
-            if current_direction == "WEST":
-                if  c-1 >= 0:
-                    c-=1
-            if current_direction == "EAST":
-                if c+1 <= maxC:
-                    c +=1
-            #print(r,c)
-            map[r][c] = "*"
-    return map 
+            if current_direction == "SOUTH" and rover_row_pos + 1 < number_of_rows:
+                rover_row_pos += 1
+            elif current_direction == "NORTH" and rover_row_pos - 1 >= 0:
+                rover_row_pos -= 1
+            elif current_direction == "WEST" and rover_col_pos - 1 >= 0:
+                rover_col_pos -= 1
+            elif current_direction == "EAST" and rover_col_pos + 1 <= number_of_cols:
+                rover_col_pos += 1
+            print(rover_row_pos, rover_col_pos)
+            map[rover_row_pos][rover_col_pos] = "*"
 
-def generate_rover_path(rover_id, commands, map_info):
+    return map
+
+# def generate_rover_path(rover_id, commands, map_info):
     
-    dig = False
-    current_row = 1
-    current_col = 0
-    current_direction = "SOUTH"
-    max_rows = map_info[0]
-    max_cols = map_info[1]
+#     dig = False
+#     current_row = 1
+#     current_col = 0
+#     current_direction = "SOUTH"
+#     max_rows = map_info[0]
+#     max_cols = map_info[1]
 
-    # Need an Update Rover Path Function 
-    total_time = 0
-    for move in commands[rover_id]:
-        if move == 'L' or move == 'R':
-            current_direction = rotate_rover(current_direction, move)
+#     # Need an Update Rover Path Function 
+#     total_time = 0
+#     for move in commands[rover_id]:
+#         if move == 'L' or move == 'R':
+#             current_direction = rotate_rover(current_direction, move)
             
 
-        elif move == 'M':
+#         elif move == 'M':
             
-            if current_direction == "SOUTH":
-                if current_row < max_rows:
-                    current_row += 1
-            elif current_direction == "WEST":
-                if current_col >= 2:
-                    current_col -= 2
-            elif current_direction == "NORTH":
-                if current_row > 1:
-                    current_row -= 1
-            elif current_direction == "EAST":
-                if current_col < ((3 - 1) * 2):
-                    current_col += 2
+#             if current_direction == "SOUTH":
+#                 if current_row < max_rows:
+#                     current_row += 1
+#             elif current_direction == "WEST":
+#                 if current_col >= 2:
+#                     current_col -= 2
+#             elif current_direction == "NORTH":
+#                 if current_row > 1:
+#                     current_row -= 1
+#             elif current_direction == "EAST":
+#                 if current_col < ((3 - 1) * 2):
+#                     current_col += 2
 
-        elif move == 'D':
-            dig = True
-            # Need a Check for Mine 
+#         elif move == 'D':
+#             dig = True
+#             # Need a Check for Mine 
     
-    print(f'Total time took: {total_time} seconds.')
+#     print(f'Total time took: {total_time} seconds.')
 
 def rotate_rover(current_direction, move) -> str:
     directions = ["NORTH", "EAST", "SOUTH", "WEST"]
@@ -103,20 +106,20 @@ def build_map(map_file_name):
 
     map = [[0 for i in range(cols)] for j in range(rows)]
 
-    r = 0
-    c = 0
-
-    for line in fmap.readlines():
-       c = 0
-       for x in line.split():
-           if x == "1":
-               map[r][c] = 1
-           c+=1
-       r+=1 
-
+    for row, line in enumerate(fmap.readlines()):
+        cells = line.split()
+        for col, value in enumerate(cells):
+            if value == "1":
+                map[row][col] = 1
     fmap.close()    
+
     return info, map
 
+def write_rover_path_to_file(rover_id, map):
+    file_name = f'path_{rover_id}.txt'
+    with open(file_name, 'w') as f:
+        for row in map:
+            f.write(" ".join([str(cell) for cell in row]) + "\n")
 
 def main():
     info,map = build_map("map1.txt")
@@ -128,14 +131,11 @@ def main():
 
     print("...")
 
-#def rover_movement(rover_id, commands, info, starter_map):
     new_map = rover_movement(0, commands, info, map)
     for x in new_map:
         print(x)
-    #generate_rover_path(1, commands, map)
-    #print(commands)
+    write_rover_path_to_file(1, new_map)
     # TODO ---------
-    # print path.txt
     # add mine handling
 
 if __name__ == "__main__":
